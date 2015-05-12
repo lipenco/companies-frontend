dfd47d2401c7197b9ddea8584c55ee9556da45b4
Magdalena Lipińska <mag.lipinska@gmail.com>
2015-05-13 01:47:48 +0900

    layout bugfix

--- src/require.unit.load.js
+++ src/require.unit.load.js
@@ -1,10 +1,10 @@
 define(function(require) {
   'use strict';
 
-  // require('shared/fend/progressbar-loading/tests/unit/package');
-  // require('shared/fend/input-utils/tests/unit/package');
-  // require('shared/fend/navbar/tests/unit/package');
-  // require('shared/fend/pagination/tests/unit/package');
+  require('shared/fend/progressbar-loading/tests/unit/package');
+  require('shared/fend/input-utils/tests/unit/package');
+  require('shared/fend/navbar/tests/unit/package');
+  require('shared/fend/pagination/tests/unit/package');
 
   require('app/main/tests/unit/package');
 
